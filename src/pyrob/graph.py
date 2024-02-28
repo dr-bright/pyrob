0fea21d5d96112fcb53a03bdf43d45fcc6292dc4
drBright <gkigki111@gmail.com>
2024-02-28 17:47:18 +0300

    added

--- src/pyrob/graph.py
+++ src/pyrob/graph.py
@@ -1,3 +1,28 @@
-
+class Graph:
+    """
+    
+    Атрибуты:
+        self.vertices = [ label, ... ]
+        self.edges = [ (id1, id2[, weight]), ... ]
+    """
+    def __init__(self, vertices, edges, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.vertices = vertices
+        self.edges = edges
+    
+    def add_vertex(self, label):
+        return id
+    
+    def remove_vertex(self, id):
+        return label
+    
+    def add_edge(self, id1, id2, weight=None):
+        pass
+    
+    def remove_edge(self, id1, id2):
+        return weight
+    
+    
+            
 
         
