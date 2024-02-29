a4dfb51e0e8f53a6de71fd2fe1dffc26bfa78b65
drBright <gkigki111@gmail.com>
2024-02-29 21:18:01 +0300

    Working impl of graphs

--- src/pyrob/graph.py
+++ src/pyrob/graph.py
@@ -5,29 +5,37 @@ class Graph(set):
     
     Атрибуты:
         self.edges = [ (id1, id2), ... ]
-    """
+    """    
+    def add_edge(self, id, *ids):
+        for id2 in ids:
+            edge = tuple(sorted((id, id2)))
+            self.add(edge)
     
-    def make_directed(self):
-        self.intersection_update(map(tuple, map(sorted, self)))
+    def remove_edge(self, id, *ids, directed=False):
+        for id2 in ids:
+            self.discard((id1, id2))
+            self.discard((id2, id1))
     
-    def add_edge(self, id1, id2, directed=False):
-        edge = tuple(sorted((id1, id2)
-        if not directed:
-            edge = tuple(sorted(edge))
-        self.edges.add(edge)
-    
-    def remove_edge(self, id1, id2, *ids):
-        self.edges.discard((id1, id2))
-        if self.directed:
-            self.edges.discard((id2, id1))
+    def get_neighbors(self, id):
+        neighbors = set()
+        for edge in self:
+            edge = list(edge)
+            if id in edge:
+                edge.remove(id)
+                neighbors.add(edge.pop())
+        return neighbors
     
     def remove_vertex(self, id):
-        remove = set(filter(lambda x: id in x, self.edges))
-        self.edges -= remove
-        return remove
-
+        neighbors = self.get_neighbors(self, id)
+        self.remove_edge(id, *neighbors)
+        
 
 class SpatialGraph(Graph):
+    """
+    
+    Атрибуты:
+        self.vertices = список координат вершин
+    """
     def add_vertex(self, label, *ids):
         if not hasattr(self, 'vertices'):
             self.vertices = []
@@ -36,20 +44,35 @@ class SpatialGraph(Graph):
         self.add_edge(id, *ids)
         return id
     
-    def render2D(self, ax):
+    def render2D(self, ax, show=None):
         if ax is None:
             fig, ax = plt.subplots()
+            if show is None:
+                show = True
         ax.scatter(*zip(self.vertices))
         xs, ys = [], []
         for edge in self.edges:
             pair = map(self.vertices.__getitem__, edge)
             ax.plot(*zip(*pair))
-        return ax
+        show and plt.show()
+
 
 class WeightedGraph(Graph):
-    def edge_weight(self, id1, id2):
-        pass
+    def set_weight(self, id1, id2=None, *, w):
+        if not hasattr(self, 'weights'):
+            self.weights = {}
+        idx = id1
+        if id2 is not None:
+            idx = tuple(sorted((id1, id2)))
+        if w is None:
+            return self.weights.pop(idx, None)
+        else:
+            self.weights[idx] = w
     
-    def vertex_weight(self, id):
-        pass
+    def get_weight(self, id1, id2=None):
+        idx = id1
+        if id2 is not None:
+            idx = tuple(sorted((id1, id2)))
+        return self.weights.get(idx)
+    
     
